e51b33c282136efdf4692aae9641117365bd8091
heermasterlp <heermaster@163.com>
2015-10-12 14:04:43 +0800

    Initial Commit

---
+++ OpenCVTutorials/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCVTutorials
+//
+//  Created by PeterLiu on 10/12/15.
+//  Copyright © 2015 PeterLiu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
